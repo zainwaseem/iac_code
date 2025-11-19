c1249b9d53b4aa0503f47c8ed19579ef2841cd40
Zain Waseem <zainwaseem9371@gmail.com>
2025-11-19 18:49:34 +0500

    IAC bucket name fix run pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,10 @@
 output "region" {
-    description = "Jumphost Server region"
-    value = var.region
+  description = "Jumphost Server region"
+  value       = var.region
 }
 output "jumphost_public_ip" {
   description = "Public IP address of the EC2 jumphost"
   value       = aws_instance.ec2.public_ip
 }
+
+#
